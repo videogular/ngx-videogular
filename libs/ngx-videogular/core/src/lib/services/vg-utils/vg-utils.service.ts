e4909b75dc7f043be731e3bf917f31f4c75c07f6
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2020-06-25 16:43:22 +0200

    fix(package): Fix EVERYTHING

---
+++ libs/ngx-videogular/core/src/lib/services/vg-utils/vg-utils.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VgUtilsService {
+  /**
+   * Inspired by Paul Irish
+   * https://gist.github.com/paulirish/211209
+   */
+  static getZIndex(): number {
+    let zIndex = 1;
+    let elementZIndex: number;
+
+    const tags = document.getElementsByTagName('*');
+
+    for (let i = 0, l = tags.length; i < l; i++) {
+      elementZIndex = parseInt(window.getComputedStyle(tags[i])['z-index'], 10);
+
+      if (elementZIndex > zIndex) {
+        zIndex = elementZIndex + 1;
+      }
+    }
+
+    return zIndex;
+  }
+
+  // Very simple mobile detection, not 100% reliable
+  static isMobileDevice() {
+    // return (
+    //   typeof window.screen.orientation !== 'undefined' ||
+    //   navigator.userAgent.indexOf('IEMobile') !== -1
+    // );
+
+    // window.orientation is deprecated and we should use window.screen.orientation
+    return (
+      typeof window.orientation !== 'undefined' ||
+      navigator.userAgent.indexOf('IEMobile') !== -1
+    );
+  }
+
+  static isiOSDevice() {
+    return (
+      navigator.userAgent.match(/ip(hone|ad|od)/i) &&
+      !navigator.userAgent.match(/(iemobile)[\/\s]?([\w\.]*)/i)
+    );
+  }
+
+  static isCordova() {
+    return (
+      document.URL.indexOf('http://') === -1 &&
+      document.URL.indexOf('https://') === -1
+    );
+  }
+}
---
+++ libs/ngx-videogular/public-api.ts
@@ -0,0 +1,6 @@
+export * from "@videogular/ngx-videogular/core";
+export * from "@videogular/ngx-videogular/buffering";
+export * from "@videogular/ngx-videogular/overlay-play";
+export * from "@videogular/ngx-videogular/ima-ads";
+export * from "@videogular/ngx-videogular/controls";
+export * from "@videogular/ngx-videogular/streaming";
