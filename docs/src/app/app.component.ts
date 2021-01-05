4fed45b170ed4d42e54d4af4f64f714119195a51
Jaime Oliveira <jaime.amo18@gmail.com>
2021-01-05 11:13:21 +0000

    ci(GH-Pages): Rebuild pages at b50f23e

--- docs/src/app/app.component.ts
+++ docs/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {SafeResourceUrl, DomSanitizer} from '@angular/platform-browser';
+import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
 
 export interface IVgDemo {
   label: string;
@@ -9,34 +9,33 @@ export interface IVgDemo {
 @Component({
   selector: "ngx-videogular-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
   currentDemo: SafeResourceUrl;
   selectedDemo: IVgDemo;
   isDemoEnabled = false;
-  title = 'ngx-videogular';
+  title = "ngx-videogular";
 
   demos: IVgDemo[] = [
     {
       label: "Picture-in-Picture",
       src:
-        "https://videogular.github.io/videogular2-showroom/#/master-player?standalone=true",
+        "https://videogular.github.io/ngx-videogular-showroom/#/master-player?standalone=true",
     },
     {
       label: "Cue points",
       src:
-        "https://videogular.github.io/videogular2-showroom/#/cue-points-player?standalone=true",
+        "https://videogular.github.io/ngx-videogular-showroom/#/cue-points-player?standalone=true",
     },
     {
       label: "Play Anything",
       src:
-        "https://videogular.github.io/videogular2-showroom/#/custom-media?standalone=true",
+        "https://videogular.github.io/ngx-videogular-showroom/#/custom-media?standalone=true",
     },
     {
       label: "Advertisement",
       src:
-        "https://videogular.github.io/videogular2-showroom/#/google-ima-player?standalone=true",
+        "https://videogular.github.io/ngx-videogular-showroom/#/google-ima-player?standalone=true",
     },
   ];
 
